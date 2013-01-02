e00627c097a7c2a6650d560bf532f24de3423597
Dale Sedivec <dale@codefu.org>
2013-01-01 23:38:28 -0600

    Stop using Vundle as a submodule
    I realized I haven't been updating Vundle this whole time, and I can
    never remember how to use submodules, and they weren't really buying
    me anything here.

--- .bootstrap-dotfiles.sh
+++ .bootstrap-dotfiles.sh
@@ -4,3 +4,6 @@ git clone git@github.com:dsedivec/dotfiles.git ~/dotfiles
 rsync -av ~/dotfiles/ ~/
 rm -rf ~/dotfiles
 echo '*' >> ~/.git/info/exclude
+install -d ~/.vim/bundle
+cd ~/.vim/bundle
+git clone git://github.com/gmarik/vundle.git
