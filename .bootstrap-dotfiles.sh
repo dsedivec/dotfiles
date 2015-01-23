d1f32fa49db9e922c28212b425262240c9b01989
Dale Sedivec <dale@codefu.org>
2015-01-22 20:28:43 -0600

    Don't lock me out with sshd
    See added comment for how that's possible!

--- .bootstrap-dotfiles.sh
+++ .bootstrap-dotfiles.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 set -ex || exit 1
 git clone git@github.com:dsedivec/dotfiles.git ~/dotfiles
-rsync -av ~/dotfiles/ ~/
+# -p is probably bad because it may change permissions on ~ such that
+# sshd will no longer accept your authorized_keys.  Hence we're not
+# just using -a.
+#
+# -o and -g shouldn't be necessary either, for that matter.
+rsync -rltDv ~/dotfiles/ ~/
 rm -rf ~/dotfiles
 echo '*' >> ~/.git/info/exclude
 install -d ~/.vim/bundle
